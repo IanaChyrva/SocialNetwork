47efed84b07b62d758e62337eaf6545258ea6be2
Iana Chyrva <yanachirva@gmail.com>
2020-08-14 12:35:40 +0300

    Social network simple layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img src="https://miro.medium.com/max/8256/1*5lpiSFo6j5dhrr6Z6RFd8Q.jpeg" alt="" className="main-image" />
+            <div>
+                ava+descr
+      </div>
+            <div>
+                My post
+        <div>New post</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
